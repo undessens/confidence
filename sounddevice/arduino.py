7a7ebea1c7d90fbbdda59ae4b07b2c4d3d02be3d
UnDesSens <assoundessens@gmail.com>
2018-12-15 19:39:28 +0100

    Add arduino , Serial connection

---
+++ sounddevice/arduino.py
@@ -0,0 +1,103 @@
+from serial import Serial
+
+class arduino:
+
+	def __init__(self, _port, _baudrate):
+		self.port = _port
+		self.baudrate = _baudrate
+		# Num of channels, led and buttons
+		self.numButtons = 8
+		
+		#Buttons state
+		self.buttonState = []
+
+		self.ledState = []
+
+		self.initState()
+
+		self.s  =  None
+
+	def initState(self):
+
+		print "Init state"
+
+		for i in range(self.numButtons) :
+			self.buttonState.append(False)
+		
+		for i in range(self.numButtons) :
+			self.ledState.append(0)
+
+		print self.buttonState
+		print self.ledState
+
+	def connect(self):
+
+		try :
+			self.s = Serial(self.port, self.baudrate)
+			print "connected"
+		except :
+			print "impossible de se connecter"
+
+
+
+
+
+
+
+	def readSerial(self):
+
+		bytye = ''
+		byte = self.s.read(1)
+		newValue = []
+
+		for i in range(0, numButtons):
+			self.newValue[i] = byte & (1<<i)
+
+		for i in range(0, self.numButtons):
+			
+			# Button is pressed
+			if( self.newValue[i] and not(self.buttonState[i])):
+				self.buttonPressed(i)
+
+
+			#update values
+			self.buttonState[i] = newValue[i]
+
+	def buttonPressed(self, index):
+
+		print "buttonPressed : "+str(index)
+
+
+	def sendLedState(self):
+
+		msg = ""
+
+		for i in range(0, self.numButtons):
+			msg += chr(self.ledState[i] + 48 )
+
+		msg += 'A'
+		self.s.write(msg)
+		print "message send : "+msg
+
+	def setLedState( self, index , state): 
+
+		if(index>=0 and index<self.numButtons ):
+			if(state >=0 and state < 3):
+				self.ledState[index] = state
+
+		print "ledState modified"
+		print self.ledState
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sounddevice/main.py
+++ sounddevice/main.py
@@ -1,7 +1,8 @@
 from recorder import recorder
 from player import player
+from arduino import arduino
 import time
-import serial
+
 
 def main():
 
@@ -9,19 +10,51 @@ def main():
 	rec = recorder("", None ,1)
 	play = player("", None, 1)
 
-	ser = serial.Serial('/dev/tty.usbserial', 9600)
-	
-	time.sleep(1)
-	print "start recording"
+	ard = arduino("/dev/tty.usbserial-AH06LA61", 115200)
 
-	##chose record file name that doesn't exist
-	nameFile = "truc6.wav"
+	ard.connect()
 
-	
-	rec.recordFile(nameFile)
+	print "Welcome"
 
+	#rec.recordFile(nameFile)
 	##play the same file
-	play.playFile(nameFile)
+	#play.playFile(nameFile)
+
+	# Wait for arduino to initialize arduino serial 
+	time.sleep(2)
+	print "GO"
+
+
+	ard.setLedState(0, 2)
+	ard.setLedState(1, 2)
+	ard.setLedState(2, 2)
+	ard.sendLedState()
+
+	time.sleep(7)
+
+	ard.setLedState(0, 0)
+	ard.setLedState(1, 0)
+	ard.setLedState(2, 0)
+	ard.setLedState(3, 2)
+	ard.setLedState(4, 2)
+	ard.setLedState(5, 2)
+	ard.setLedState(6, 2)
+	ard.setLedState(7, 1)
+	ard.sendLedState()
+
+	time.sleep(3)
+
+	print "end"
+
+
+
+
+
+	# while True:
+
+	# 	arduino.readSerial()
+	# 	time.sleep(0.1)
+
 
 
 
